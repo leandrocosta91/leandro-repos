6ce4d87bfe489c7f7eaef3d15c101f0457bfe2a2
Leandro Costa <leandro.costa91@outlook.com>
2017-12-17 18:20:28 -0400

    first commit

---
+++ src/components/Repos.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-layout>
+    <div slot="header" class="toolbar">
+      <q-toolbar-title :padding="0">
+        Repositórios - Quasar Framework v{{ $q.version }}
+      </q-toolbar-title>
+    </div>
+    <h4>Últimos 30 repositórios publicados</h4>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
